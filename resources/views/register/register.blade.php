063bd80056b2818cce226cfaa72e6bd2b10e3530
denayutiara <denayutiara@gmail.com>
2022-04-01 10:18:04 +0700

    Progress register

---
+++ resources/views/register/register.blade.php
@@ -0,0 +1,73 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="css/style.css"> --}}
+    <title>TelusurTasik</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/templatemo-style.css">
+
+</head>
+    <body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+    <h1>Wisata</h1>
+	<div class="container h-100">
+    		<div class="row h-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+
+						<div class="text-center mt-4">
+							<h1 class="h2">Get started</h1>
+							<p class="lead">
+								Start creating the best possible user experience for you customers.
+							</p>
+						</div>
+
+						<div class="card">
+							<div class="card-body">
+								<div class="m-sm-4">
+									<form>
+										<div class="form-group">
+											<label>First Name</label>
+											<input class="form-control form-control-lg" type="text" name="firstname" placeholder="Enter your first name">
+										</div>
+										<div class="form-group">
+											<label>Last Name</label>
+											<input class="form-control form-control-lg" type="text" name="lastname" placeholder="Enter your last name">
+										</div>
+										<div class="form-group">
+											<label>Email</label>
+											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email">
+										</div>
+										<div class="form-group">
+											<label>Password</label>
+											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter your password">
+										</div>
+										<div class="text-center mt-3">
+											<a href="index.html" class="btn btn-lg btn-primary">Create an account</a>
+											<!-- <button type="submit" class="btn btn-lg btn-primary">Sign up</button> -->
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
 });
 
-nyoba 
+Route::get('/wisata', function () {
+    return view('wisata');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/help', function () {
+    return view('help');
+});
